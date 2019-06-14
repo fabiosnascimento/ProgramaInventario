e19d923c72c2f95df33274796d8f0966a849be46
Fabio Nascimento <fabiogtr@hotmail.com>
2019-06-14 10:54:32 -0300

    Classe Itens criada

---
+++ src/model/entities/Itens.java
@@ -0,0 +1,73 @@
+package model.entities;
+
+public class Itens {
+
+	private int id;
+	private String nome;
+	private int quantidade;
+	private String marca;
+	
+	public Itens() {
+	}
+
+	public Itens(int id, String nome, int quantidade, String marca) {
+		this.id = id;
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.marca = marca;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Itens other = (Itens) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+}
