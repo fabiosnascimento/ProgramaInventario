32ce8163fd4a0a20cd95d09c641fd1c8ced4bfbc
Fabio Nascimento <fabiogtr@hotmail.com>
2019-06-17 11:46:34 -0300

    Criada tela About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -20,7 +21,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Programa de Inventário");
 			primaryStage.show();
@@ -28,10 +29,13 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 
 	public static void main(String[] args) {
 
 		launch(args);
 	}
-
 }
