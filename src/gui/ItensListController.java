bec372e66a096c75045eb9fb4b56768d9def95b5
Fabio Nascimento <fabiogtr@hotmail.com>
2019-06-18 11:37:42 -0300

    Criada a caixa de diálogo ItensForm e montado o design

--- src/gui/ItensListController.java
+++ src/gui/ItensListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Itens;
 import model.services.ItensService;
@@ -37,8 +46,9 @@ public class ItensListController implements Initializable {
 	private ObservableList<Itens> obsList;
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("onBtNovoAction");
+	public void onBtNovoAction(ActionEvent e) {
+		Stage parentStage = Utils.currentStage(e);
+		createDialogForm("/gui/ItensForm.fxml", parentStage);;
 	}
 	
 	public void setItensService(ItensService service) {
@@ -69,4 +79,21 @@ public class ItensListController implements Initializable {
 		tableViewItens.setItems(obsList);
 	}
 
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Insira os dados do equipamento");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro carregando tela", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
--- src/model/services/ItensService.java
+++ src/model/services/ItensService.java
@@ -1,18 +1,17 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.ItensDao;
 import model.entities.Itens;
 
 public class ItensService {
 	
+	private ItensDao dao = DaoFactory.createItensDao();
+	
 	public List<Itens> findAll() {
-		List<Itens> list = new ArrayList<>();
-		list.add(new Itens(1, "Teclado USB novo", 3, null));
-		list.add(new Itens(2, "Teclado USB usado", 5, null));
-		list.add(new Itens(3, "Mouse USB novo", 4, null));
-		return list;
+		return dao.findAll();
 	}
 
 }
